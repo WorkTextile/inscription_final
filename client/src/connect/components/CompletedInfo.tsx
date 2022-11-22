1b8fdad5d811d4e8b5a120a72e0cede82e76d6c8
mojtaba-kazmi <kazmi.saidmojtaba@gmail.com>
2022-11-22 10:26:33 +0100

    Completed info page has been correct to display all the finish form result

--- client/src/connect/components/CompletedInfo.tsx
+++ client/src/connect/components/CompletedInfo.tsx
@@ -1,22 +1,17 @@
 import React from 'react';
-import { useForm } from "react-hook-form";
 import { useNavigate } from "react-router-dom";
 import { useFormData } from '../../context/UserContext';
 
 const CompletedInfo:  React.FC = (): JSX.Element =>{
     const { userData } = useFormData();
-    const { handleSubmit } = useForm({ defaultValues: userData });
     const navigate =  useNavigate();
-
-    const submitData = (data: any) => {
-        console.info(data);
-        // Submit data to the server
-    };
+    
+    
 
     return (
         <>
         <h1>Félicitation 🎉</h1>
-        <p className='JSON'>{JSON.stringify(userData)}</p>
+        <pre>{JSON.stringify(userData, null, 2)}</pre>
         <button onClick={() => navigate("/")}>Finalisez votre inscription pro</button>
         </>
         
